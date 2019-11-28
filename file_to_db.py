9dc428bf9c41b367ea2963683d8d6c3920822020
mrbayati <mrbayati2002@gmail.com>
2019-11-28 23:08:21 +0330

    use functions in func.py

--- file_to_db.py
+++ file_to_db.py
@@ -2,27 +2,11 @@
 
 import sqlite3
 from sys import argv
+from funcs import lname_exr, lname_remove_spaces, create_table, ins_to_table
 
 date = argv[1]
 con = sqlite3.connect('dbs/%s.db'%date)
 c = con.cursor()
-
-def lname_remove_spaces(lname):
-    s = ''
-    for c in lname:
-        if c not in [' ', '-', ',', '.', ':']:
-            s += c
-    return s
-
-def lname_exr(lname):
-    l = 0
-    while lname[l] in [' ', '.', ':', '\t'] and l < len(lname):
-        l += 1
-    r = len(lname) - 1
-    while lname[r] in [' ', '.', ':', '\t'] and r >= 0:
-        r -= 1
-    r += 1
-    return lname[l:r]
 
 l = 1
 r = 99
@@ -31,11 +15,9 @@ for cf in range(l, r + 1):
     with open('tests/test%s.txt'%str(cf), 'r') as f:
         for i in f:
             lname, count, taraz, tmp = i.split('\t')
-            lname = lname_remove_spaces(lname_exr(lname))
-            count = int(count)
-            taraz = int(taraz)
-            c.execute('CREATE TABLE IF NOT EXISTS %s(count INT, taraz INT)'%lname)
-            c.execute('INSERT INTO %s VALUES(%i, %i)'%(lname, count, taraz))
+            count ,taraz = int(count), int(taraz)
+            create_table(lname, con, c)
+            ins_to_table(lname, count, taraz, con, c)
     print(cf)
 
 con.commit()

22af22b4eded0a18e12373d09d85415305d95c57
mrbayati <mrbayati2002@gmail.com>
2019-11-28 23:11:30 +0330

    remove commented codes

--- test.py
+++ test.py
@@ -6,9 +6,8 @@ date = '13980901'
 base_url = 'http://www.kanoon.ir/Public/TopStudents?main=1&group=1&date=%s&year=99'%date
 post_url = 'http://www.kanoon.ir/Public/TopStudentsWorkbook'
 
-# extract_data(date, base_url, post_url)
-# lname = 'آمارواحتمال'
-# cad = 1
-date, lname, cad = argv[1], argv[2], int(argv[3])
+up = open('up.txt', 'r')
+date, lname, cad = up.read().split(' ')
+cad = int(cad)
 res = a_lesson(date, lname, cad)
-print(cad, ': ', res)
+print(lname, ': ', cad, ': ', res)
